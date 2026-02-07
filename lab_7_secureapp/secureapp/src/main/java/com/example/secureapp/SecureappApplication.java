c9ccfb68d01f78c38eefe5540a156592c6af9089
Chanon18820 <chanonwilaikum@gmail.com>
2026-02-07 20:00:25 +0700

    Edit.
    From Demo to Secureapp

--- lab_7_secureapp/secureapp/src/main/java/com/example/secureapp/SecureappApplication.java
+++ lab_7_secureapp/secureapp/src/main/java/com/example/secureapp/SecureappApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.secureapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SecureappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SecureappApplication.class, args);
 	}
 
 }
