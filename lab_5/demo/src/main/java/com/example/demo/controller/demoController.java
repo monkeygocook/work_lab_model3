45012b755138a504bd1f96ad5a0b5fa3ba1f1e5e
monkeygocook <72518648+monkeygocook@users.noreply.github.com>
2026-02-07 19:08:32 +0700

    creat_controller_and_add_code

---
+++ lab_5/demo/src/main/java/com/example/demo/controller/demoController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class demoController {
+    // Serve the index.html page
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Refers to src/main/resources/templates/index.html
+    }
+    // Serve the registration.html page
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+}
