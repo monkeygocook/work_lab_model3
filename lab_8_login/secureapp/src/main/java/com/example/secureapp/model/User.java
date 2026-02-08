9324abd64d24e7def27368f374127506a3a37648
FRoGFraGy <frogdev05@gmail.com>
2026-02-09 03:53:00 +0700

    Add Field

--- lab_8_login/secureapp/src/main/java/com/example/secureapp/model/User.java
+++ lab_8_login/secureapp/src/main/java/com/example/secureapp/model/User.java
@@ -4,36 +4,49 @@ public class User {
 
     private String username;
     private String password;
-    private String role; // Add a role field
+    private String role;
 
-    public User(String username, String password, String role) {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(
+            String username,
+            String password,
+            String role,
+            String firstName,
+            String lastName,
+            String email
+    ) {
         this.username = username;
         this.password = password;
         this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
-    // Getters and Setters
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
