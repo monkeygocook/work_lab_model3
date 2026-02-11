ec937f216d337bc8dec725ec14b07dfd48945019
monkeygocook <72518648+monkeygocook@users.noreply.github.com>
2026-02-11 09:31:24 +0700

    updata_lab_5

---
+++ lab_5/mvcwebdemoas/src/test/java/com/example/mvcwebdemoas/MvcwebdemoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcwebdemoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcwebdemoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
